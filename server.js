c1106d343898b148d94965919e026aaa5ebdfaf2
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-05-18 10:19:43 +0300

    add some changes

--- server.js
+++ server.js
@@ -74,15 +74,6 @@ app.use("/api/payment-methods", paymentMethods);
 app.use("/api/partners", partners);
 // app.use('/', mainRoute);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 console.log(process.env.NODE_ENV);
 const port = process.env.PORT || 4000;
 
